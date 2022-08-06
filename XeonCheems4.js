0c2de51604457905b8503aa12c934333cacc3717
Vihanga - YT <96816258+vihangayt0@users.noreply.github.com>
2022-08-06 08:11:12 -0700

    Update XeonCheems4.js

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -515,7 +515,7 @@ jumlahharian = `${dataa.value}`
 	    let setting = global.db.data.settings[botNumber]
 	    if (new Date() * 1 - setting.status > 1000) {
 		let uptime = await runtime(process.uptime())
-		await XeonBotInc.setStatus(`${XeonBotInc.user.name} | Runtime : ${runtime(uptime)}`)
+		await XeonBotInc.setStatus(`${XeonBotInc.user.name} | Runtime : ${runtime(process.uptime())}`)
 		setting.status = new Date() * 1
 	    }
 	}
